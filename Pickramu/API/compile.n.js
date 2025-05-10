2497daf97d0b9950a3b3ec5d12714c3890741cdf
Sugisaku8 <sumisj522@gmail.com>
2025-05-10 18:49:30 +0900

    Update tag attributes in compile.n.js to use class syntax for improved consistency and maintainability.

--- Pickramu/API/compile.n.js
+++ Pickramu/API/compile.n.js
@@ -80,20 +80,20 @@ function convertToHtml(inputText) {
 }
 
 const inputText = `
-@tag n1,unit [open]
+@tag n1 class=unit [open]
 @tag question [open]
 約10万年前ごろのものと見られる人類の痕跡が日本列島から見つかりました。
 この頃の人類は、動物を狩って生活していました。
 @tag question [close]
 @btn id=btn1 class=button-next 次へ
-@tag n1,unit [close]
-@tag n2,unit [open]
+@tag n1 class=unit [close]
+@tag n2 class=unit [open]
 @tag question [open]
 約 1 万年前、日本列島が大陸から分離しました。
 紀元前 3000 年ごろ、稲作が日本列島に伝わりました。
 @tag question [close]
 @btn id=btn2 class=button-next 次へ
-@tag n2,unit [close]
+@tag n2 class=unit [close]
 @script on=btn1 [open]
 dom.Tag("n1").style.display('none','auto');
 dom.Tag("n2").style.display('block','auto');
