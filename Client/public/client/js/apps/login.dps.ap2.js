4c1e65ce3acff902874f4dc4038c5fe6896924ca
Sugisaku8 <sumisj522@gmail.com>
2025-06-30 18:39:55 +0900

    chore: Bump version numbers for Deep-School Family to 25C930X1, login to 0.0.8, and menu to 0.0.16; update copyright information

--- Client/public/client/js/apps/login.dps.ap2.js
+++ Client/public/client/js/apps/login.dps.ap2.js
@@ -33,7 +33,7 @@ export function appInit(shell) {
         </div>
       </div>
       <div class="copyright-container">
-        <p class="copyright chalk-text" data-lang-key="copyright">(c) 2022-2025 Carnation Studio v0.1.2</p>
+        <p class="copyright chalk-text" data-lang-key="copyright">(c) 2022-2025 Carnation Studio v0.2.6 25C930X1</p>
       </div>
     </div>
   
