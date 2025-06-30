ec675d92b2607222b06b3c59980f339dfbfbe880
Sugisaku8 <sumisj522@gmail.com>
2025-06-30 16:05:04 +0900

    fix: Update privacy policy and terms of service links to use .html extension; enable inline math rendering in viewer script

--- Pickramu/viewer/script.js
+++ Pickramu/viewer/script.js
@@ -866,7 +866,7 @@ body{
             renderMathInElement(document.body, {
               delimiters: [
                 {left: "$$", right: "$$", display: true},
-                {left: "$", right: "$", display: false},
+                {left: "$", right: "$", display: true},
                 {left: "\\(", right: "\\)", display: true},
                 {left: "\\[", right: "\\]", display: true}
               ],
