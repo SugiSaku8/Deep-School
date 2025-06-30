1bb65f5bd0eb0dad101f833a8bbd83b2f21327fe
Sugisaku8 <sumisj522@gmail.com>
2025-06-30 19:56:21 +0900

    chore: Update version numbers for Deep-School Family to 25C946X1 and pickramu to 1.0.49

--- Client/public/client/js/apps/menu.dps.ap2.js
+++ Client/public/client/js/apps/menu.dps.ap2.js
@@ -40,7 +40,7 @@ export function appInit(shell) {
     </div>
     
     <div class="copyright-container horizontal-copyright">
-      <p class="copyright chalk-text left-align" data-lang-key="copyright">(c) 2022-2025 Carnation Studio v0.2.7 25C935X1</p>
+      <p class="copyright chalk-text left-align" data-lang-key="copyright">(c) 2022-2025 Carnation Studio v0.2.7 25C946X1</p>
     </div>
   
   <style>
