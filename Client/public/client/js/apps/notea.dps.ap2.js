5c7d3ef46ea28cf7938296bf5dddeb6e922a8f13
Sugisaku8 <sumisj522@gmail.com>
2025-08-26 16:27:03 +0900

    fix:set height

--- Client/public/client/js/apps/notea.dps.ap2.js
+++ Client/public/client/js/apps/notea.dps.ap2.js
@@ -20,8 +20,8 @@ export function appInit(shell) {
   <div style="height:100%,width:100%">
   <iframe
 
-  width="100%"
-  height="100%"
+  width="100%;"
+  height="700px;"
   src="https://sugisaku8.github.io/Deep-School/client/js/apps/notea/index.html">
 </iframe>
 </div>`
